0494571d21d29b84a1a31f55e095eb233623aae8
bartwe2 <barwer2@gmail.com>
2018-12-05 13:10:26 +0100

    move cards

--- src/main/java/server/endpoints/CardEndpoints.java
+++ src/main/java/server/endpoints/CardEndpoints.java
@@ -1,12 +1,15 @@
 package server.endpoints;
 
+import domain.DomainBoard;
 import domain.DomainCard;
 import domain.DomainList;
+import domain.DomainUser;
 import io.javalin.Javalin;
 import lombok.val;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.PersistenceException;
+import java.util.stream.Collectors;
 
 public class CardEndpoints {
     public static void create(Javalin app, EntityManagerFactory entityManagerFactory) {
@@ -52,6 +55,39 @@ public class CardEndpoints {
                 val result = tmp.createQuery("SELECT t FROM Card t where t.id=:id", DomainCard.class).setParameter("id", id).getSingleResult();
                 ctx.json(result);
             } catch (Exception ex) {
+                ctx.result(ex.toString());
+            } finally {
+                tmp.close();
+            }
+        });
+
+        app.patch("boards/:board/lists/:from/cards/:id/move/:to", ctx -> {
+            AuthEndpoint.enableAuthCORSFix(ctx);
+            int boardId = Integer.valueOf(ctx.pathParam("board"));
+            int id = Integer.valueOf(ctx.pathParam("id"));
+            int fromId = Integer.valueOf(ctx.pathParam("from"));
+            int toId = Integer.valueOf(ctx.pathParam("to"));
+            val tmp = entityManagerFactory.createEntityManager();
+            val managerTransaction = tmp.getTransaction();
+            managerTransaction.begin();
+            try {
+                val result = tmp.createQuery("SELECT t FROM Board t where t.id=:id", DomainBoard.class).setParameter("id", boardId).getSingleResult();
+                DomainList fromList =  result.getLists().stream().filter(item -> item.getId().equals(fromId)).collect(Collectors.toList()).get(0);
+                DomainList toList =  result.getLists().stream().filter(item -> item.getId().equals(toId)).collect(Collectors.toList()).get(0);
+                DomainCard domainCard = fromList.getCards().stream().filter(item -> item.getId().equals(id)).collect(Collectors.toList()).get(0);
+
+                fromList.getCards().remove(domainCard);
+                tmp.persist(result);
+                managerTransaction.commit();
+
+                toList.getCards().add(domainCard);
+                tmp.persist(result);
+                managerTransaction.begin();
+                managerTransaction.commit();
+
+                ctx.result(result.toString());
+
+            } catch (IndexOutOfBoundsException ex) {
                 ctx.result(ex.toString());
             } finally {
                 tmp.close();
