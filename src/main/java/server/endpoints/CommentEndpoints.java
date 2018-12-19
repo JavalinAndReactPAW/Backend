613dabeb7917016df48420479a671f8ede23b1d1
bartwe2 <barwer2@gmail.com>
2018-12-19 14:44:45 +0100

    delete comment

--- src/main/java/server/endpoints/CommentEndpoints.java
+++ src/main/java/server/endpoints/CommentEndpoints.java
@@ -23,11 +23,39 @@ public class CommentEndpoints {
 
             try {
                 val result = tmp.createQuery("SELECT t FROM Board t where t.id=:id", DomainBoard.class).setParameter("id", boardId).getSingleResult();
-                DomainList list =  result.getLists().stream().filter(item -> item.getId().equals(listId)).collect(Collectors.toList()).get(0);
+                DomainList list = result.getLists().stream().filter(item -> item.getId().equals(listId)).collect(Collectors.toList()).get(0);
                 DomainCard card = list.getCards().stream().filter(item -> item.getId().equals(id)).collect(Collectors.toList()).get(0);
                 card.getComments().add(DomainComment.builder()
                         .value(domainComment.getValue())
                         .addedBy(domainUser.getLogin()).build());
+                tmp.persist(result);
+                managerTransaction.commit();
+            } catch (PersistenceException pe) {
+                ctx.result(pe.toString());
+            } catch (Exception ex) {
+                ctx.result(ex.toString());
+            } finally {
+                tmp.close();
+            }
+        });
+
+        app.delete("boards/:idboard/lists/:idlist/cards/:idcard/comments/:idcomment", ctx -> {
+            AuthEndpoint.enableAuthCORSFix(ctx);
+            int boardId = Integer.valueOf(ctx.pathParam("idboard"));
+            int listId = Integer.valueOf(ctx.pathParam("idlist"));
+            int cardId = Integer.valueOf(ctx.pathParam("idcard"));
+            int id = Integer.valueOf(ctx.pathParam("idcomment"));
+
+            val tmp = entityManagerFactory.createEntityManager();
+            val managerTransaction = tmp.getTransaction();
+            managerTransaction.begin();
+
+            try {
+                val result = tmp.createQuery("SELECT t FROM Board t where t.id=:id", DomainBoard.class).setParameter("id", boardId).getSingleResult();
+                DomainList list = result.getLists().stream().filter(item -> item.getId().equals(listId)).collect(Collectors.toList()).get(0);
+                DomainCard card = list.getCards().stream().filter(item -> item.getId().equals(cardId)).collect(Collectors.toList()).get(0);
+                DomainComment comment = card.getComments().stream().filter(item -> item.getId().equals(id)).collect(Collectors.toList()).get(0);
+                card.getComments().remove(comment);
                 tmp.persist(result);
                 managerTransaction.commit();
             } catch (PersistenceException pe) {
